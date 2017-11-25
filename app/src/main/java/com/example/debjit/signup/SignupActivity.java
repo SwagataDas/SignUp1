00d05eef40d8378bb7045c1b521b4dbe4e289823
Swagata Das <swagatadas.wb@gmail.com>
2017-11-25 16:08:44 +0530

    init commit

--- app/src/main/java/com/example/debjit/signup/SignupActivity.java
+++ app/src/main/java/com/example/debjit/signup/SignupActivity.java
@@ -5,7 +5,10 @@ import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
@@ -13,6 +16,7 @@ import android.provider.MediaStore;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -20,47 +24,59 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.basgeekball.awesomevalidation.AwesomeValidation;
 import com.basgeekball.awesomevalidation.ValidationStyle;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Calendar;
 
-public class SignupActivity extends AppCompatActivity {
-    Button signupbutton, asubutton;
+public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
+    Button signupButton, asubutton;
     ImageButton imageButton;
     ImageView imageView1;
-    EditText dob,firstname,lastname,emailid;
+    EditText dob, firstname, lastname, emailid;
     DatePickerDialog datePickerDialog;
     private AwesomeValidation awesomeValidation;
-    private int PICK_IMAGE_REQUEST =1;
-    private int IMAGE_CAPTURE=1;
-    Integer REQUEST_CAMERA=1, SELECT_FILE=0;
+    private int PICK_IMAGE_REQUEST = 1;
+    private int IMAGE_CAPTURE = 1;
+    Integer REQUEST_CAMERA = 1, SELECT_FILE = 0;
     ImageView imageView;
+    private String userChoosenTask;
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    //@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        signupButton = (Button) findViewById(R.id.signup);
+        //email validation
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+        awesomeValidation.addValidation(this, R.id.email, Patterns.EMAIL_ADDRESS, R.string.emailerror);
+
+        signupButton.setOnClickListener(this);
+
+
         imageView1 = (ImageView) findViewById(R.id.imageView);
         imageView1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent();
-                SelectImage();
+                selectImage();
             }
         });
-        signupbutton = (Button) findViewById(R.id.signup);
         dob = (EditText) findViewById(R.id.dob1);
-        firstname =(EditText) findViewById(R.id.firstname);
-        lastname =(EditText) findViewById(R.id.lastname);
-        emailid = (EditText) findViewById(R.id.emailid);
+        firstname = (EditText) findViewById(R.id.firstname);
+        lastname = (EditText) findViewById(R.id.lastname);
+        emailid = (EditText) findViewById(R.id.email);
         imageButton = (ImageButton) findViewById(R.id.dobbtn);
-        //email validation
-        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
-        awesomeValidation.addValidation(this, R.id.emailid, Patterns.EMAIL_ADDRESS, R.string.emailerror);
-
         imageButton.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
@@ -77,13 +93,13 @@ public class SignupActivity extends AppCompatActivity {
                         dob.setText(dayOfMonth + "/"
                                 + (monthOfYear + 1) + "/" + year);
                     }
-                },mDay, mMonth, mYear);
+                }, mDay, mMonth, mYear);
                 datePickerDialog.show();
             }
         });
         Intent intent2 = getIntent();
 
-        signupbutton.setOnClickListener(new View.OnClickListener() {
+        signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent3 = new Intent(SignupActivity.this, UserprofileActivity.class);
@@ -91,60 +107,136 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case Utility.MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (userChoosenTask.equals("Camera"))
+                        cameraIntent();
+                    else if (userChoosenTask.equals("Choose from gallery"))
+                        galleryIntent();
+                } else {
+                    //code for deny
+                }
+                break;
         }
+    }
 
-    private void SelectImage(){
+    private void selectImage() {
 
-        final CharSequence[] items={"Camera", "Gallery", "Cancel"};
-        android.support.v7.app.AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(SignupActivity.this);
-        builder.setTitle("Add Image");
+        final CharSequence[] items = {"Camera", "Choose from gallery", "Cancel"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(SignupActivity.this);
+        builder.setTitle("Add Photo!");
         builder.setItems(items, new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                if(items[i].equals("Camera")){
-                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(intent,REQUEST_CAMERA);
+            public void onClick(DialogInterface dialog, int item) {
+                boolean result = Utility.checkPermission(SignupActivity.this);
 
-                }else if (items[i].equals("Gallery")){
-                    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    intent.setType("/image");
-                    startActivityForResult(intent.createChooser(intent, "Select File"), SELECT_FILE);
-                    intent.setType("image/*");
-                    intent.setAction(Intent.ACTION_GET_CONTENT);
-                    startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+                if (items[item].equals("Camera")) {
+                    userChoosenTask = "Camera";
+                    if (result)
+                        cameraIntent();
 
-                }else if (items[i].equals("Cancel")){
-                    dialogInterface.dismiss();
+                } else if (items[item].equals("Choose from gallery")) {
+                    userChoosenTask = "Choose from gallery";
+                    if (result)
+                        galleryIntent();
+
+                } else if (items[item].equals("Cancel")) {
+                    dialog.dismiss();
                 }
             }
         });
         builder.show();
     }
 
+    private void galleryIntent() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);//
+        startActivityForResult(Intent.createChooser(intent, "Select File"), SELECT_FILE);
+    }
+
+    private void cameraIntent() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_CAMERA);
+    }
+
     @Override
-    public  void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode, resultCode,data);
-        if(requestCode== Activity.RESULT_OK){
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-            if (resultCode==REQUEST_CAMERA){
-
-                Bundle bundle = data.getExtras();
-                final Bitmap bmp = (Bitmap) bundle.get("data");
-                imageView.setImageBitmap(bmp);
-            }else if (requestCode==SELECT_FILE){
-                Uri selectedImageUri = data.getData();
-                imageView.setImageURI(selectedImageUri);
-            }
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == SELECT_FILE)
+                onSelectFromGalleryResult(data);
+            else if (requestCode == REQUEST_CAMERA)
+                onCaptureImageResult(data);
         }
     }
 
-        //asubutton.setOnClickListener(new View.OnClickListener() {
-           // @Override
-           // public void onClick(View view) {
-             //   Intent intent3 = new Intent(SignupActivity.this, UserprofileActivity.class);
-             //   startActivity(intent3);
-          //  }
-       // });
+    private void onCaptureImageResult(Intent data) {
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+
+        File destination = new File(Environment.getExternalStorageDirectory(),
+                System.currentTimeMillis() + ".jpg");
+
+        FileOutputStream fo;
+        try {
+            destination.createNewFile();
+            fo = new FileOutputStream(destination);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        imageView.setImageBitmap(thumbnail);
+    }
+
+    @SuppressWarnings("deprecation")
+    private void onSelectFromGalleryResult(Intent data) {
+
+        Bitmap bm = null;
+        if (data != null) {
+            try {
+                bm = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), data.getData());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        imageView.setImageBitmap(bm);
+    }
+
+
+
+    @Override
+    public void onClick(View view) {
+        if (view == signupButton) {
+            submitForm();
+        }
+    }
+
+    private void submitForm() {
+        if (awesomeValidation.validate()) {
+            Toast.makeText(SignupActivity.this, "Registration Successfull", Toast.LENGTH_LONG).show();
+        }
 
     }
+
+    //asubutton.setOnClickListener(new View.OnClickListener() {
+    // @Override
+    // public void onClick(View view) {
+    //   Intent intent3 = new Intent(SignupActivity.this, UserprofileActivity.class);
+    //   startActivity(intent3);
+    //  }
+    // });
+}
 
